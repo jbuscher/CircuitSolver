36a13e856c193d032941a96257c143a090b4728d
jbuscher <jbuscher@uw.edu>
2015-12-29 12:50:13 -0800

    OnHover checkpoint for drawing setup. Neighbors and clickables set up

--- circuitGenerator.js
+++ circuitGenerator.js
@@ -17,7 +17,7 @@ var COLS=14;
         for(var i = 0; i < ROWS; i++) {
             htmlString += "<tr id='row," + i + "' class='circuit-row'>"
             for(var j = 0; j < COLS; j++) {
-                htmlString += "<td id='node," + i +","+ j + "' class='circuit-node'>" + "&#8226;"+ "</td>";
+                htmlString += "<td id='node," + i +","+ j + "' class='circuit-node clickable'>" + "&#8226;"+ "</td>";
                 count++;
             }
             htmlString += "</tr>"
@@ -32,7 +32,7 @@ var COLS=14;
         for(var i = 0; i < nodes.length; i++) {
             nodes[i].onclick = function() {
                 var neighbors = getNeighbors(this);
-                setOnHovers(neighbors);
+                setOnHovers(this, neighbors);
                 setNodesSecondOnClick(neighbors);
             };
         }
@@ -42,9 +42,18 @@ var COLS=14;
 
     }
 
-    function setOnHovers(neighbors) {
+    function setOnHovers(clickedNode, neighbors) {
+        var clickedID = clickedNode.id;
         var nodes = $(".circuit-node");
-        for()
+        for(var i = 0; i < nodes.length; i++) {
+            var node = nodes[i];
+            if (neighbors.indexOf(node.id) > -1) {
+            } else if (node.id == clickedID) {
+            } else {
+                nodes[i].classList.add('not-clickable');
+                nodes[i].classList.remove('clickable');
+            }
+        }
     }
 
     function getNeighbors(node) {
@@ -56,29 +65,25 @@ var COLS=14;
         console.log(row + "," + col);
         //top neighbor
         if(row > 0) {
-            var top = $("#node," + (row-1) + "," + col);
-            neighbors.push(top);
+            neighbors.push("node," + (row-1) + "," + col);
         } else {
             neighbors.push(null);
         }
         //right neighbor
         if(col < COLS - 1) {
-            var right = $("#node," + row + "," + (col+1));
-            neighbors.push(right);
+            neighbors.push("node," + row + "," + (col+1));
         } else {
             neighbors.push(null);
         }
         //bottom neighbor
         if(row < ROWS-1) {
-            var bottom = $("#node," + (row+1) + "," + col);
-            neighbors.push(bottom);
+            neighbors.push("node," + (row+1) + "," + col);
         } else {
             neighbors.push(null);
         }
         //left neighbor
         if(col > 0) {
-            var left = $("#node," + row + "," + (col-1));
-            neighbors.push(left);
+            neighbors.push("node," + row + "," + (col-1));
         } else {
             neighbors.push(null);
         }
